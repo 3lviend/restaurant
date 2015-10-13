b30470507fce2594332d829d00b14e04c023e66b
3lviend <henryss_10@yahoo.com>
2015-10-13 12:32:23 +0700

    generate access token and update gem

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,13 @@ class User < ActiveRecord::Base
   # validates_presence_of :password, :email
   validates :password, length: { minimum: 6}
   # validates :email, uniqueness: true
+
+  before_create :generate_authentication_token!
+
+  def generate_authentication_token!
+    begin
+      self.access_token =  Devise.friendly_token
+    end while self.class.exists?(access_token: access_token)
+  end
+  
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,73 +11,75 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151005074256) do
+ActiveRecord::Schema.define(version: 20151013051726) do
 
   create_table "ingredients", force: :cascade do |t|
-    t.string   "name"
-    t.decimal  "quantity"
-    t.string   "detail_of_quantity"
-    t.datetime "created_at",         null: false
-    t.datetime "updated_at",         null: false
+    t.string   "name",               limit: 255
+    t.decimal  "quantity",                       precision: 10
+    t.string   "detail_of_quantity", limit: 255
+    t.datetime "created_at",                                    null: false
+    t.datetime "updated_at",                                    null: false
   end
 
   create_table "items", force: :cascade do |t|
-    t.string   "name"
-    t.text     "detail_of_item"
-    t.decimal  "price"
-    t.integer  "portion"
-    t.string   "type_of_item"
-    t.boolean  "available",      default: true
-    t.datetime "created_at",                    null: false
-    t.datetime "updated_at",                    null: false
+    t.string   "name",           limit: 255
+    t.text     "detail_of_item", limit: 65535
+    t.decimal  "price",                        precision: 10
+    t.integer  "portion",        limit: 4
+    t.string   "type_of_item",   limit: 255
+    t.boolean  "available",      limit: 1,                    default: true
+    t.datetime "created_at",                                                 null: false
+    t.datetime "updated_at",                                                 null: false
   end
 
   create_table "line_items", force: :cascade do |t|
-    t.integer  "item_id"
-    t.float    "quantity"
-    t.integer  "order_id"
-    t.datetime "created_at",                 null: false
-    t.datetime "updated_at",                 null: false
-    t.boolean  "complete",   default: false
+    t.integer  "item_id",    limit: 4
+    t.float    "quantity",   limit: 24
+    t.integer  "order_id",   limit: 4
+    t.datetime "created_at",                            null: false
+    t.datetime "updated_at",                            null: false
+    t.boolean  "complete",   limit: 1,  default: false
   end
 
   create_table "orders", force: :cascade do |t|
-    t.integer  "table_id"
-    t.string   "status"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.integer  "table_id",   limit: 4
+    t.string   "status",     limit: 255
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
   end
 
   create_table "roles", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string   "name"
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+    t.string   "name",       limit: 255
   end
 
   create_table "tables", force: :cascade do |t|
-    t.string   "name"
-    t.boolean  "available",  default: true
-    t.datetime "created_at",                null: false
-    t.datetime "updated_at",                null: false
+    t.string   "name",       limit: 255
+    t.boolean  "available",  limit: 1,   default: true
+    t.datetime "created_at",                            null: false
+    t.datetime "updated_at",                            null: false
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
+    t.string   "email",                  limit: 255, default: "", null: false
+    t.string   "encrypted_password",     limit: 255, default: "", null: false
+    t.string   "reset_password_token",   limit: 255
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",          limit: 4,   default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
-    t.integer  "role_id"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
+    t.string   "current_sign_in_ip",     limit: 255
+    t.string   "last_sign_in_ip",        limit: 255
+    t.integer  "role_id",                limit: 4
+    t.datetime "created_at",                                      null: false
+    t.datetime "updated_at",                                      null: false
+    t.string   "access_token",           limit: 255, default: ""
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["access_token"], name: "index_users_on_access_token", using: :btree
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
